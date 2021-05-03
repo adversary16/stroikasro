aaf11de58c8a02e9f319b3b28203e24aacc1542e
Nikita E <nikita.e@fora-soft.com>
2021-05-04 02:29:05 +0300

    ++

--- webclient/const/const.js
+++ webclient/const/const.js
@@ -1,4 +1,4 @@
-export const BASEURL = 'http://localhost:4001/v1';
+export const BASEURL = process.env.BASEURL || 'http://localhost:4001/v1';
 export const SERVERSIDE_BASEURL = 'http://core:4001/v1';
 
 export const API_URLS = {
